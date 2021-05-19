a2bfc6c33d77dc1bb4efa00b5e4cd4057da626b1
lahavana <36070872+lahavana@users.noreply.github.com>
2021-05-19 18:49:25 +0200

    Update Run_WVD_Language_Installer.ps1

--- Run_WVD_Language_Installer.ps1
+++ Run_WVD_Language_Installer.ps1
@@ -6,20 +6,40 @@
  $appName = 'optimize'
  $drive = 'C:'
  New-Item -Path $drive -Name $appName -ItemType Directory -ErrorAction SilentlyContinue
- $LocalPath = $drive + '\' + $appName 
+ $LocalPath = $drive + '\' + $appName
+ New-Item -Path $LocalPath -Name "language" -ItemType Directory -ErrorAction SilentlyContinue
  set-Location $LocalPath
 
-Invoke-WebRequest "https://software-download.microsoft.com/download/pr/19041.1.191206-1406.vb_release_CLIENTLANGPACKDVD_OEM_MULTI.iso" -OutFile "19041.1.191206-1406.vb_release_CLIENTLANGPACKDVD_OEM_MULTI.iso"
-Invoke-WebRequest "https://software-download.microsoft.com/download/pr/19041.1.191206-1406.vb_release_amd64fre_FOD-PACKAGES_OEM_PT1_amd64fre_MULTI.iso" -OutFile "19041.1.191206-1406.vb_release_amd64fre_FOD-PACKAGES_OEM_PT1_amd64fre_MULTI.iso"
-Invoke-WebRequest "https://software-download.microsoft.com/download/pr/19041.508.200905-1327.vb_release_svc_prod1_amd64fre_InboxApps.iso" -OutFile "19041.508.200905-1327.vb_release_svc_prod1_amd64fre_InboxApps.iso"
+Invoke-WebRequest "https://software-download.microsoft.com/download/pr/19041.1.191206-1406.vb_release_CLIENTLANGPACKDVD_OEM_MULTI.iso" -OutFile "01_LANGUAGEPACK.iso"
+Invoke-WebRequest "https://software-download.microsoft.com/download/pr/19041.1.191206-1406.vb_release_amd64fre_FOD-PACKAGES_OEM_PT1_amd64fre_MULTI.iso" -OutFile "02_FODPACK.iso"
+Invoke-WebRequest "https://software-download.microsoft.com/download/pr/19041.508.200905-1327.vb_release_svc_prod1_amd64fre_InboxApps.iso" -OutFile "03_InboxApps.iso"
 
 
 $isos = Get-Childitem -Path $LocalPath -Include *.iso -Recurse
+
+
 foreach ($iso in $isos.name)
     {
     $Mount = Mount-DiskImage -ImagePath $($LocalPath+"\"+$iso)
     write-host $($LocalPath+"\"+$iso)
     $driveLetter = ($mount | Get-Volume).DriveLetter
+        
+        if (Test-Path $($driveLetter+":\x64\langpacks"))
+            {
+            Copy-Item $($driveLetter+":\x64\langpacks\*") -Destination $($LocalPath+"\language") -Recurse
+            Copy-Item $($driveLetter+":\LocalExperiencePack\*") -Destination $($LocalPath+"\language") -Recurse
+            }
+        if (Test-Path $($driveLetter+":\metadata"))
+            {
+            Copy-Item $driveLetter -Destination $($LocalPath+"\language\*") -Recurse
+            }
+        if (Test-Path $($driveLetter+":\amd64fre"))
+            {
+            Copy-Item $($driveLetter+":\amd64fre\*") -Destination $($LocalPath+"\language") -Recurse
+            }
     }
 
+
+
 write-host 'AIB Customization: Finished OS Optimizations script'
+

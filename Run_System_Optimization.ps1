5d9be9aa175f041f547654aeff35676780e8dceb
lahavana <36070872+lahavana@users.noreply.github.com>
2021-05-19 13:48:37 +0200

    Add files via upload

---
+++ Run_System_Optimization.ps1
@@ -0,0 +1,14 @@
+﻿#Timezone
+$Id = 'W. Europe Standard Time'
+set-timezone $Id
+
+#Language
+#https://docs.microsoft.com/en-us/azure/virtual-desktop/language-packs#prerequisites
+<#language packs:
+https://software-download.microsoft.com/download/pr/19041.1.191206-1406.vb_release_CLIENTLANGPACKDVD_OEM_MULTI.iso
+https://software-download.microsoft.com/download/pr/19041.1.191206-1406.vb_release_amd64fre_FOD-PACKAGES_OEM_PT1_amd64fre_MULTI.iso
+https://software-download.microsoft.com/download/pr/19041.508.200905-1327.vb_release_svc_prod1_amd64fre_InboxApps.iso
+#>
+
+#Keyboard
+

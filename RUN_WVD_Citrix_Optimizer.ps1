5253d3100c4f28295e73b5202cc625fd4448d702
lahavana <36070872+lahavana@users.noreply.github.com>
2021-06-09 11:18:20 +0200

    Update RUN_WVD_Citrix_Optimizer.ps1
    Changed XML Path with variable

--- RUN_WVD_Citrix_Optimizer.ps1
+++ RUN_WVD_Citrix_Optimizer.ps1
@@ -1,10 +1,10 @@
  #OS Optimizations for WVD
  #Change the $osOptURL to your own created in the script "Create_WVD_Optimization
  #Copy to C:\optimize
- write-host 'AIB Customization: OS Optimizations for WVD with Citrix Optimizer'
- $appName = 'optimize'
- $drive = 'C:'
- New-Item -Path $drive -Name $appName -ItemType Directory -ErrorAction SilentlyContinue
+ write-host 'AIB Customization: OS Optimizations for WVD'
+ $appName = 'optimize3'
+ $drive = 'C:\'
+ New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
  $LocalPath = $drive + '\' + $appName 
  set-Location $LocalPath
  #$osOptURL = 'https://sawvdautimation.blob.core.windows.net/wvdoptimization/CitrixOptimizer_202106.zip'
@@ -13,10 +13,11 @@
  $outputPath = $LocalPath + '\' + $osOptURLexe
  Invoke-WebRequest -Uri $osOptURL -OutFile $outputPath
  write-host 'AIB Customization: Starting OS Optimizations with Citrix Optimizer'
- Expand-Archive -LiteralPath 'C:\\Optimize\\CitrixOptimizer.zip' -DestinationPath $($Localpath+"\"+$($osOptURLexe.Split('.')[0])) -Force -Verbose
+ Expand-Archive -LiteralPath $outputPath -DestinationPath $($Localpath+"\"+$($osOptURLexe.Split('.')[0])) -Force -Verbose
  #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
  Set-Location -Path "$LocalPath\\CitrixOptimizer"
+ $Template = "$LocalPath\\CitrixOptimizer"
 
  #Run Citrix Optimize
-    .\CtxOptimizerEngine.ps1 "C:\optimize\CitrixOptimizer\Templates\Citrix_Windows_10_2009.xml" -Mode Execute
+    .\CtxOptimizerEngine.ps1 "$Template\\Templates\Citrix_Windows_10_2009.xml" -Mode Execute
     write-host 'AIB Customization: Finished OS Optimizations for WVD with Citrix Optimizer'

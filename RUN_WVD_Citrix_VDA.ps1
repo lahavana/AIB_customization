0cbbf420f777992c083070ffddebc88306c0cdf5
lahavana <36070872+lahavana@users.noreply.github.com>
2021-06-15 16:28:14 +0200

    Create RUN_WVD_Citrix_VDA.ps1

---
+++ RUN_WVD_Citrix_VDA.ps1
@@ -0,0 +1,3 @@
+#@echo off
+write-host  "*** Citrix VDA Installation ***"
+& .\VDAServerSetup.exe /quiet /enable_remote_assistance /disableexperiencemetrics /virtualmachine /INSTALL_MCSIO_DRIVER /enable_real_time_transport /enable_hdx_ports /enable_hdx_udp_ports /includeadditional "Citrix Supportability Tools","Machine Identity Service","Citrix User Profile Manager WMI Plugin" /exclude "User Personalization layer","Citrix Files for Outlook","Citrix Files for Windows","Citrix Telemetry Service","Citrix Personalization for App-V - VDA" /components vda /mastermcsimage 

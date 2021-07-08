1886d9fe3d8f0683514db5f4787f799e66fa63cc
lahavana <36070872+lahavana@users.noreply.github.com>
2021-07-08 16:01:12 +0200

    Update RUN_WVD_Citrix_VDA.ps1

--- RUN_WVD_Citrix_VDA.ps1
+++ RUN_WVD_Citrix_VDA.ps1
@@ -1,3 +1,9 @@
-#@echo off
-write-host  "*** Citrix VDA Installation without WorkspaceApp ***"
-& .\VDAServerSetup.exe /quiet /enable_remote_assistance /disableexperiencemetrics /virtualmachine /INSTALL_MCSIO_DRIVER /enable_real_time_transport /enable_hdx_ports /enable_hdx_udp_ports /includeadditional "Citrix Supportability Tools","Machine Identity Service","Citrix User Profile Manager WMI Plugin" /exclude "User Personalization layer","Citrix Files for Outlook","Citrix Files for Windows","Citrix Telemetry Service","Citrix Personalization for App-V - VDA" /components vda /mastermcsimage 
+#VDA Installation
+ write-host 'Install Citrix VDA with WorkspaceApp'
+ $appName = 'VDA'
+ $drive = 'C:\temp\'
+ New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
+ $LocalPath = $drive + '\' + $appName 
+ set-Location $LocalPath
+write-host  "*** Citrix VDA Installation with WorkspaceApp ***"
+& .\VDAServerSetup_2106.exe /quiet /enable_remote_assistance /disableexperiencemetrics /virtualmachine /INSTALL_MCSIO_DRIVER /enable_real_time_transport /enable_hdx_ports /enable_hdx_udp_ports /includeadditional "Citrix Supportability Tools","Machine Identity Service","Citrix User Profile Manager WMI Plugin" /exclude "User Personalization layer","Citrix Files for Outlook","Citrix Files for Windows","Citrix Telemetry Service","Citrix Personalization for App-V - VDA" /components vda,plugins /mastermcsimage 
